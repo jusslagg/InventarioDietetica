0afe7ea29ff60f2c9f9c12515390b2999a775651
Jesus Gil <lagg312@gmail.com>
2025-01-30 15:43:45 -0300

    se agrega el 30%

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -281,6 +281,7 @@ const Checkout = () => {
                   <option value={0}>Sin descuento</option>
                   <option value={10}>10% de descuento</option>
                   <option value={20}>20% de descuento</option>
+                  <option value={30}>30% de descuento</option>
                 </select>
               </label>
 
